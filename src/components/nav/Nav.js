7ecbd65815cb7b619539bfe962ef5df1aa806e93
ibnubs <ibnubs@gmail.com>
2020-06-30 02:58:54 +0700

    [ibnu]-initial commit feature homepage

--- src/components/nav/Nav.js
+++ src/components/nav/Nav.js
@@ -1,19 +1,32 @@
 import React, { Fragment } from 'react';
-import { Layout, Menu } from 'antd';
-import '../../App.css';
+import { Layout, Menu, Row, Avatar } from 'antd';
+import './nav.css';
 
 const { Header } = Layout;
 
 const Nav = () => {
     return (
         <Fragment>
-            <Header className="header">
-                <div className="logo" />
-                <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['2']}>
-                    <Menu.Item key="1">nav 1</Menu.Item>
-                    <Menu.Item key="2">nav 2</Menu.Item>
-                    <Menu.Item key="3">nav 3</Menu.Item>
+            <Header className="header" className="navBar" >
+                <div className="logo" >
+                    <img src={require('../../assets/images/tinpet-navbar-logo.svg')} />
+                </div>
+                <Row justify="end">
+                <Menu  mode="horizontal"  collapsible="false" style={{borderBottom: 'none'}}>
+                    <Menu.Item key="1" >
+                        <a><img src={require('../../assets/images/message-icon.svg')} /></a>
+                    </Menu.Item>
+                    <Menu.Item key="2">
+                        <a><img src={require('../../assets/images/notification-icon.svg')} /></a>
+                    </Menu.Item>
+                    <Menu.Item key="3">
+                        <a>
+                            <Avatar size={35} src={require('../../assets/images/michael-dam-mEZ3PoFGs_k-unsplash.jpg')} />
+                            {/* <img  style={{width:'35px', height:'35px'}} /></a> */} 
+                        </a>
+                    </Menu.Item>
                 </Menu>
+                </Row>
             </Header>
         </Fragment>
     );
