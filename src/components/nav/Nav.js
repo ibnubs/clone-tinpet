e8a8376e9a82c214e11fe57168373d2683aa808a
ibnubs <ibnubs@gmail.com>
2020-06-30 12:58:03 +0700

    [ibnu]-update feature homepage

--- src/components/nav/Nav.js
+++ src/components/nav/Nav.js
@@ -7,21 +7,22 @@ const { Header } = Layout;
 const Nav = () => {
     return (
         <Fragment>
-            <Header className="header" className="navBar" >
+            <Header className="header navBar" >
                 <div className="logo" >
-                    <img src={require('../../assets/images/tinpet-navbar-logo.svg')} />
+                    <img src={require('../../assets/images/tinpet-navbar-logo.svg')} alt='tinpet-logo' />
                 </div>
                 <Row justify="end">
                 <Menu  mode="horizontal"  collapsible="false" style={{borderBottom: 'none'}}>
                     <Menu.Item key="1" >
-                        <a><img src={require('../../assets/images/message-icon.svg')} /></a>
+                        <a href='/#'><img src={require('../../assets/images/message-icon.svg')}  alt='message-icon' /></a>
+                        
                     </Menu.Item>
                     <Menu.Item key="2">
-                        <a><img src={require('../../assets/images/notification-icon.svg')} /></a>
+                        <a href='/#'><img src={require('../../assets/images/notification-icon.svg')}  alt='notif-icon' /></a>
                     </Menu.Item>
                     <Menu.Item key="3">
-                        <a>
-                            <Avatar size={35} src={require('../../assets/images/michael-dam-mEZ3PoFGs_k-unsplash.jpg')} />
+                        <a href='/#'>
+                            <Avatar size={35} src={require('../../assets/images/michael-dam-mEZ3PoFGs_k-unsplash.jpg')}  alt='avatar-icon' />
                             {/* <img  style={{width:'35px', height:'35px'}} /></a> */} 
                         </a>
                     </Menu.Item>
