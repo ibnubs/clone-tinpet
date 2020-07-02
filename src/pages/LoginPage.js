22825070ce906f02aba5e86c3c3d3df1d0f1602d
radhiyahfahra <radhiyah.fahra09@gmail.com>
2020-07-02 17:24:24 +0700

    [radhiyah] - fixing merge request

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -121,7 +121,7 @@ const LoginPage = () => {
 
 	          <Form.Item>
 	            <Button type="primary" htmlType="submit">
-	                Sign Up
+	                Login
 	            </Button>
 	          </Form.Item>
 			 
