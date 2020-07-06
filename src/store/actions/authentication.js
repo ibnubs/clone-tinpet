d301372b495527522d144e75186366cce3d68540
angga-22 <anggasp.unib@gmail.com>
2020-07-06 19:43:21 +0700

    [angga] - integrating with api register

--- src/store/actions/authentication.js
+++ src/store/actions/authentication.js
@@ -3,10 +3,10 @@ import axios from 'axios';
 const baseUrl ='https://product-tinpet-app.herokuapp.com';
 
 export const register = data => async dispatch => {
+	console.log("data", data)
 	try{
 		const res = await axios.post(`${baseUrl}/api/v1/users/register`, data)
 		console.log('respond', res)		
-		localStorage.setItem('newUser', res.data.data.token)
 		console.log('Register Success!')
 		dispatch({
 			type: REGISTER_SUCCESS
