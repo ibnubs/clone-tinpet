9872df3752253e314a006e2c713b9320eb9b14d3
angga-22 <anggasp.unib@gmail.com>
2020-07-07 13:52:09 +0700

    [angga] - fetch api register and login for push history

--- src/components/modals/CreatePostModal.js
+++ src/components/modals/CreatePostModal.js
@@ -1,4 +1,4 @@
-import React, {useState, useCallback, useEffect} from 'react';
+import React, {useState,} from 'react'; // useCallback, useEffect
 import { Form, Input, Button,Select, Modal, message, Upload } from 'antd';
 import { LoadingOutlined, PlusOutlined, } from '@ant-design/icons';
 import { useSelector  } from 'react-redux';
@@ -16,7 +16,7 @@ const CreatePostModal = (props) => {
 		imgUrl: localStorage.getItem('userAvatar')
 	})
 	 //store
-	const isAuthenticate = useSelector(state => state.auth.isAuthenticate)
+	// const isAuthenticate = useSelector(state => state.auth.isAuthenticate)
 	const updateStatus = useSelector( state => state.auth.updateStatus)
 	// const closeModal = useCallback(
 	// 	() => {
