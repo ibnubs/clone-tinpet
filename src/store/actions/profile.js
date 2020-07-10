168ab054e74c3bfe525d646cb45539e207a95a5c
ibnubs <ibnubs@gmail.com>
2020-07-10 23:45:15 +0700

    [ibnu]-fetch profile

---
+++ src/store/actions/profile.js
@@ -0,0 +1,26 @@
+import axios from 'axios';
+
+const baseUrl = 'https://product-tinpet-app.herokuapp.com';
+
+export const getProfile = () => async (dispatch) => {
+    const token = localStorage.getItem('token')
+    try {
+        const res = await axios.get(`${baseUrl}/api/v1/users`, {
+            headers: {
+                authorization:token
+            }
+        })
+        console.log(res, 'ini res geprofile')
+        console.log(res.data, 'ini res data profile')
+        console.log(res.data.data, 'ini res data data get profile')
+        dispatch({
+            type: 'GET_PROFILE',
+            payload: res.data.data 
+        })
+    } catch (error) {
+        console.log(error, 'error Get profile')
+        dispatch({
+            type: 'GET_PROFILE_FAILED'
+        })
+    }
+}
